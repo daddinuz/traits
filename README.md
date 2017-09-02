06ed148dc50d9e6a0e0659710d25ad84231330b3
daddinuz <daddinuz@gmail.com>
2017-09-02 15:19:38 +0200

    Version 2.1.0 (Broken compatibility)

--- README.md
+++ README.md
@@ -1,69 +1,83 @@
 Traits
 ======
 
-A light-weight unit test framework written in ISO C (C99).
-
-## Framework Overview
-
-In order to define a test:
-
-```C
-TRAITS(_Case) {
-    /* Your assertions here */
-}
-```
-
-where `_Case` is the test name and must be a valid C identifier.  
-In order to register tests, write your main function like this:
-
-```C
-int main() {
-    traits_run(_Case);
-    traits_skip(_Case);
-    /* ... */
-    return 0;
-}
-```
-
-where `_Case` must match a test case previously declared using `TRAITS`.
-
-***Notice***:
-- before every test `traits_run` will call a function (that must be implemented by the user) which signature is:  
-  `void traits_setup(void);`
-- after every test `traits_run` will call a function (that must be implemented by the user) which signature is:  
-  `void traits_teardown(void);`
+Traits is an assertion library written in C99.
+It's an header only library so you just have to include
+`traits.h` in your code and your ready for bugs hunting. 
 
 ## Assertion Summary
+
+Below are listed all the assertion macros in the library.
+Every macro is "overloaded" and can be called specifying 
+a custom message in the same way as you would do using printf.  
+
+That means that you can write something like this:
+```C
+/* Just assert without specifying a message */
+traits_assert(5 == 5);
+
+/* Assert specifying a message */
+int x = 5, y = 6;
+traits_assert(x == y, "Ops! Expected to be %d got %d", x, y);
+```
+
+#### Basic
+
+```C
+traits_assert(x);
+```
 
 #### Boolean
 
 ```C
-assert(_Expression)
-assert_true(_Expression)
-assert_false(_Expression)
+assert_true(x);
+assert_false(x);
 ```
 
 #### Numerical
 
 ```C
-assert_equal(_Expected, _Actual)
-assert_not_equal(_Expected, _Actual)
-assert_less_equal(_Expected, _Actual)
-assert_greater_equal(_Expected, _Actual)
-assert_less(_Expected, _Actual)
-assert_greater(_Expected, _Actual)
+assert_equal(e, a);
+assert_not_equal(e, a);
+assert_greater(e, a);
+assert_greater_equal(e, a);
+assert_less(e, a);
+assert_less_equal(e, a);
 ```
 
 #### Pointer
 
 ```C
-assert_null(_Pointer)
-assert_not_null(_Pointer)
+assert_null(x);
+assert_not_null(x);
 ```
 
 #### String
 
 ```C
-assert_string_equal(_Expected, _Actual)
-assert_string_not_equal(_Expected, _Actual)
+assert_string_equal(e, a);
+assert_string_not_equal(e, a);
+```
+
+## Helpers
+
+Traits defines also some helpers to make easier and safer your custom assertion:
+
+```C
+eq(e, a)        /* ((e) == (a)) */
+ne(e, a)        /* ((e) != (a)) */
+gt(e, a)        /* ((e)  > (a)) */
+ge(e, a)        /* ((e) >= (a)) */
+lt(e, a)        /* ((e)  < (a)) */
+le(e, a)        /* ((e) <= (a)) */
+
+as(T, x)        /* It's simply a cast: ((T)(x)) */
+
+/* Those are just a combination of the above */
+eq_as(T, e, a)  /* eq(as(T, e), as(T, a)) */
+ne_as(T, e, a)  /* ne(as(T, e), as(T, a)) */
+gt_as(T, e, a)  /* gt(as(T, e), as(T, a)) */
+ge_as(T, e, a)  /* ge(as(T, e), as(T, a)) */
+lt_as(T, e, a)  /* lt(as(T, e), as(T, a)) */
+le_as(T, e, a)  /* le(as(T, e), as(T, a)) */
 ```
--- examples/example.c
+++ examples/example.c
@@ -3,7 +3,7 @@
  *
  * Author: daddinuz
  * email:  daddinuz@gmail.com
- * Date:   August 27, 2017 
+ * Date:   September 02, 2017
  */
 
 #include "traits.h"
@@ -12,32 +12,86 @@
  *
  */
 int main() {
+    /* Basic */
+    traits_assert(true);
+    traits_assert(true, "My amazing %s assertion message%c", "formatted", '!');
+
+    traits_assert(eq_as(size_t, 1U, 1.35f));
+    traits_assert(eq_as(size_t, 1U, 1.35f), "My amazing %s assertion message%c", "formatted", '!');
+
+    traits_assert(ne_as(
+                          double, 1L, 1.35f));
+    traits_assert(ne_as(
+                          double, 1L, 1.35f), "My amazing %s assertion message%c", "formatted", '!');
+
+    traits_assert(gt_as(
+                          char, 99, 'a'));
+    traits_assert(gt_as(
+                          char, 99, 'a'), "My amazing %s assertion message%c", "formatted", '!');
+
+    traits_assert(ge_as(
+                          unsigned int, 1, 0));
+    traits_assert(ge_as(
+                          unsigned int, 1, 0), "My amazing %s assertion message%c", "formatted", '!');
+
+    traits_assert(lt_as(
+                          long int, -1, 1U));
+    traits_assert(lt_as(
+                          long int, -1, 1U), "My amazing %s assertion message%c", "formatted", '!');
+
+    traits_assert(le_as(
+                          long double, -1.5f, 0x10));
+    traits_assert(le_as(
+                          long double, -1.5f, 0x10), "My amazing %s assertion message%c", "formatted", '!');
+
     /* Boolean */
     assert_true(true);
+    assert_true(true, "My amazing %s assertion message%c", "formatted", '!');
+
     assert_false(false);
+    assert_false(false, "My amazing %s assertion message%c", "formatted", '!');
 
     /* Numerical */
     assert_equal(5, 5);
+    assert_equal(5, 5, "My amazing %s assertion message%c", "formatted", '!');
+
     assert_not_equal(5, 6);
+    assert_not_equal(5, 6, "My amazing %s assertion message%c", "formatted", '!');
+
     assert_greater(6, 5);
+    assert_greater(6, 5, "My amazing %s assertion message%c", "formatted", '!');
+
     assert_greater_equal(5, 5);
     assert_greater_equal(6, 5);
-    assert_less(4, 5);
+    assert_greater_equal(5, 5, "My amazing %s assertion message%c", "formatted", '!');
+
+    assert_less(5, 6);
+    assert_less(5, 6, "My amazing %s assertion message%c", "formatted", '!');
+
     assert_less_equal(5, 5);
-    assert_less_equal(4, 5);
+    assert_less_equal(5, 6);
+    assert_less_equal(5, 5, "My amazing %s assertion message%c", "formatted", '!');
 
     /* Pointer */
     assert_null(NULL);
+    assert_null(NULL, "My amazing %s assertion message%c", "formatted", '!');
     {
         int tmp = 0;
         assert_not_null(&tmp);
+        assert_not_null(&tmp, "My amazing %s assertion message%c", "formatted", '!');
     }
 
     /* String */
     {
-        char *tmp = "Hello, World!";
-        assert_string_equal(tmp, tmp);
+        const char *const e = "Hello", *const a = "Hello";
+        assert_string_equal(e, a);
+        assert_string_equal(e, a, "My amazing %s assertion message%c", "formatted", '!');
     }
-    assert_string_not_equal("Hello World", "Hello, World!");
+    {
+        const char *const e = "Hello!", *const a = "hell0!";
+        assert_string_not_equal(e, a);
+        assert_string_not_equal(e, a, "My amazing %s assertion message%c", "formatted", '!');
+    }
+
     return 0;
 }
