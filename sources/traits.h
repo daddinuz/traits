e81e69d1f400edc9a9bc3160b8fecd2f542a1bd2
daddinuz <daddinuz@gmail.com>
2019-11-04 16:24:16 +0100

    deps

--- sources/traits.h
+++ sources/traits.h
@@ -28,6 +28,8 @@
 
 #pragma once
 
+#include <trace/trace.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -45,9 +47,6 @@ extern "C" {
 
 #define __TRAITS_CAT_IMPL_(x, ...)                  x ## __VA_ARGS__
 #define __TRAITS_CAT(x, ...)                        __TRAITS_CAT_IMPL_(x, __VA_ARGS__)
-
-#define __TRAITS_TO_STRING_IMPL_(x)                 #x
-#define __TRAITS_TO_STRING(x)                       __TRAITS_TO_STRING_IMPL_(x)
 
 #define __TRAITS_MORE_THAN_ONE_IMPL_(/**/   \
         _1, _2, _3, _4, _5, _6, _7, _8, \
@@ -123,23 +122,23 @@ extern "C" {
 /*
  * Assertions framework
  */
-static void
-__traits_assert(bool condition, size_t line, const char *file, const char *assertion, const char *message, ...)
-__attribute__((__format__(__printf__, 5, 6)));
+static inline void
+__traits_assert(bool condition, const char *__trace, const char *__assertion, const char *__message, ...)
+__attribute__((__format__(__printf__, 4, 5)));
 
-inline void
-__traits_assert(bool condition, size_t line, const char *file, const char *assertion, const char *message, ...) {
+static inline void
+__traits_assert(const bool condition, const char *const __trace, const char *const __assertion, const char *const __message, ...) {
     va_list args;
     if (!condition) {
-        fprintf(stderr, "At %s:%zu\nAssertion `%s` failed.\n", file, line, assertion);
-        va_start(args, message);
-        vfprintf(stderr, message, args);
+        fprintf(stderr, "At '%s'\r\nAssertion `%s` failed.\r\n", __trace, __assertion);
+        va_start(args, __message);
+        vfprintf(stderr, __message, args);
         va_end(args);
         exit(1);
     }
 }
 
-#define __traits_helper_x(A, x, ...)                __traits_assert((x), __LINE__, __FILE__, A, __VA_ARGS__)
+#define __traits_helper_x(A, x, ...)                __traits_assert((x), __TRACE__, A, __VA_ARGS__)
 #define __traits_helper_0(A, x)                     __traits_helper_x(A, x, NULL)
 #define __traits_helper_1(A, x, ...)                __traits_helper_x(A, x, __VA_ARGS__)
 #define __traits_helper(A, ...)                     __TRAITS_STATIC_IF(__TRAITS_MORE_THAN_ONE(__VA_ARGS__))(__traits_helper_0, __traits_helper_1)(A, __VA_ARGS__)
@@ -147,25 +146,21 @@ __traits_assert(bool condition, size_t line, const char *file, const char *asser
 /*
  * Basic
  */
-#define __traits_assert_x(x, ...)                   do { const bool __traits_x = (x); __traits_helper(__TRAITS_TO_STRING(x), eq(true, __traits_x), __VA_ARGS__); } while(false)
+#define __traits_assert_x(x, ...)                   do { const bool __traits_x = (x); __traits_helper(stringify(x), eq(true, __traits_x), __VA_ARGS__); } while(false)
 #define __traits_assert_0(x)                        __traits_assert_x(x, NULL)
 #define __traits_assert_1(x, ...)                   __traits_assert_x(x, __VA_ARGS__)
 #define traits_assert(...)                          __TRAITS_OVERLOAD_ONE(__traits_assert_0, __traits_assert_1, __VA_ARGS__)
-
-#define __assert_that_x(x, ...)                     do { const bool __traits_x = (x); __traits_helper(__TRAITS_TO_STRING(x), eq(true, __traits_x), __VA_ARGS__); } while(false)
-#define __assert_that_0(x)                          __assert_that_x(x, NULL)
-#define __assert_that_1(x, ...)                     __assert_that_x(x, __VA_ARGS__)
-#define assert_that(...)                            __TRAITS_OVERLOAD_ONE(__assert_that_0, __assert_that_1, __VA_ARGS__)
+#define assert_that(...)                            traits_assert(__VA_ARGS__)
 
 /*
  * Boolean
  */
-#define __assert_true_x(x, ...)                     do { const bool __traits_x = (x); __traits_helper(__TRAITS_TO_STRING(x), eq(true, __traits_x), __VA_ARGS__); } while(false)
+#define __assert_true_x(x, ...)                     do { const bool __traits_x = (x); __traits_helper(stringify(x), eq(true, __traits_x), __VA_ARGS__); } while(false)
 #define __assert_true_0(x)                          __assert_true_x(x, "Expected to be true.\n")
 #define __assert_true_1(x, ...)                     __assert_true_x(x, __VA_ARGS__)
 #define assert_true(...)                            __TRAITS_OVERLOAD_ONE(__assert_true_0, __assert_true_1, __VA_ARGS__)
 
-#define __assert_false_x(x, ...)                    do { const bool __traits_x = (x); __traits_helper(__TRAITS_TO_STRING(x), eq(false, __traits_x), __VA_ARGS__); } while(false)
+#define __assert_false_x(x, ...)                    do { const bool __traits_x = (x); __traits_helper(stringify(x), eq(false, __traits_x), __VA_ARGS__); } while(false)
 #define __assert_false_0(x)                         __assert_false_x(x, "Expected to be false.\n")
 #define __assert_false_1(x, ...)                    __assert_false_x(x, __VA_ARGS__)
 #define assert_false(...)                           __TRAITS_OVERLOAD_ONE(__assert_false_0, __assert_false_1, __VA_ARGS__)
@@ -173,32 +168,32 @@ __traits_assert(bool condition, size_t line, const char *file, const char *asser
 /*
  * Numerical
  */
-#define __assert_equal_x(e, a, ...)                 __traits_helper(__TRAITS_TO_STRING(eq(e, a)), eq(e, a), __VA_ARGS__)
+#define __assert_equal_x(e, a, ...)                 __traits_helper(stringify(eq(e, a)), eq(e, a), __VA_ARGS__)
 #define __assert_equal_0(e, a)                      __assert_equal_x(e, a, "Expected to be equal.\n")
 #define __assert_equal_1(e, a, ...)                 __assert_equal_x(e, a, __VA_ARGS__)
 #define assert_equal(...)                           __TRAITS_OVERLOAD_TWO(__assert_equal_0, __assert_equal_1, __VA_ARGS__)
 
-#define __assert_not_equal_x(e, a, ...)             __traits_helper(__TRAITS_TO_STRING(ne(e, a)), ne(e, a), __VA_ARGS__)
+#define __assert_not_equal_x(e, a, ...)             __traits_helper(stringify(ne(e, a)), ne(e, a), __VA_ARGS__)
 #define __assert_not_equal_0(e, a)                  __assert_not_equal_x(e, a, "Expected to be not equal.\n")
 #define __assert_not_equal_1(e, a, ...)             __assert_not_equal_x(e, a, __VA_ARGS__)
 #define assert_not_equal(...)                       __TRAITS_OVERLOAD_TWO(__assert_not_equal_0, __assert_not_equal_1, __VA_ARGS__)
 
-#define __assert_greater_x(e, a, ...)               __traits_helper(__TRAITS_TO_STRING(gt(e, a)), gt(e, a), __VA_ARGS__)
+#define __assert_greater_x(e, a, ...)               __traits_helper(stringify(gt(e, a)), gt(e, a), __VA_ARGS__)
 #define __assert_greater_0(e, a)                    __assert_greater_x(e, a, "Expected to be greater.\n")
 #define __assert_greater_1(e, a, ...)               __assert_greater_x(e, a, __VA_ARGS__)
 #define assert_greater(...)                         __TRAITS_OVERLOAD_TWO(__assert_greater_0, __assert_greater_1, __VA_ARGS__)
 
-#define __assert_greater_equal_x(e, a, ...)         __traits_helper(__TRAITS_TO_STRING(ge(e, a)), ge(e, a), __VA_ARGS__)
+#define __assert_greater_equal_x(e, a, ...)         __traits_helper(stringify(ge(e, a)), ge(e, a), __VA_ARGS__)
 #define __assert_greater_equal_0(e, a)              __assert_greater_equal_x(e, a, "Expected to be greater or equal.\n")
 #define __assert_greater_equal_1(e, a, ...)         __assert_greater_equal_x(e, a, __VA_ARGS__)
 #define assert_greater_equal(...)                   __TRAITS_OVERLOAD_TWO(__assert_greater_equal_0, __assert_greater_equal_1, __VA_ARGS__)
 
-#define __assert_less_x(e, a, ...)                  __traits_helper(__TRAITS_TO_STRING(lt(e, a)), lt(e, a), __VA_ARGS__)
+#define __assert_less_x(e, a, ...)                  __traits_helper(stringify(lt(e, a)), lt(e, a), __VA_ARGS__)
 #define __assert_less_0(e, a)                       __assert_less_x(e, a, "Expected to be less.\n")
 #define __assert_less_1(e, a, ...)                  __assert_less_x(e, a, __VA_ARGS__)
 #define assert_less(...)                            __TRAITS_OVERLOAD_TWO(__assert_less_0, __assert_less_1, __VA_ARGS__)
 
-#define __assert_less_equal_x(e, a, ...)            __traits_helper(__TRAITS_TO_STRING(le(e, a)), le(e, a), __VA_ARGS__)
+#define __assert_less_equal_x(e, a, ...)            __traits_helper(stringify(le(e, a)), le(e, a), __VA_ARGS__)
 #define __assert_less_equal_0(e, a)                 __assert_less_equal_x(e, a, "Expected to be less or equal.\n")
 #define __assert_less_equal_1(e, a, ...)            __assert_less_equal_x(e, a, __VA_ARGS__)
 #define assert_less_equal(...)                      __TRAITS_OVERLOAD_TWO(__assert_less_equal_0, __assert_less_equal_1, __VA_ARGS__)
@@ -206,12 +201,12 @@ __traits_assert(bool condition, size_t line, const char *file, const char *asser
 /*
  * Pointer
  */
-#define __assert_null_x(x, ...)                     do { const void *__traits_x = (x); __traits_helper(__TRAITS_TO_STRING(x), eq(NULL, __traits_x), __VA_ARGS__); } while(false)
+#define __assert_null_x(x, ...)                     do { const void *__traits_x = (x); __traits_helper(stringify(x), eq(NULL, __traits_x), __VA_ARGS__); } while(false)
 #define __assert_null_0(x)                          __assert_null_x(x, "Expected to be null.\n")
 #define __assert_null_1(x, ...)                     __assert_null_x(x, __VA_ARGS__)
 #define assert_null(...)                            __TRAITS_OVERLOAD_ONE(__assert_null_0, __assert_null_1, __VA_ARGS__)
 
-#define __assert_not_null_x(x, ...)                 do { const void *__traits_x = (x); __traits_helper(__TRAITS_TO_STRING(x), ne(NULL, __traits_x), __VA_ARGS__); } while(false)
+#define __assert_not_null_x(x, ...)                 do { const void *__traits_x = (x); __traits_helper(stringify(x), ne(NULL, __traits_x), __VA_ARGS__); } while(false)
 #define __assert_not_null_0(x)                      __assert_not_null_x(x, "Expected to be not null.\n")
 #define __assert_not_null_1(x, ...)                 __assert_not_null_x(x, __VA_ARGS__)
 #define assert_not_null(...)                        __TRAITS_OVERLOAD_ONE(__assert_not_null_0, __assert_not_null_1, __VA_ARGS__)
@@ -219,12 +214,12 @@ __traits_assert(bool condition, size_t line, const char *file, const char *asser
 /*
  * Memory
  */
-#define __assert_memory_equal_x(s, e, a, ...)       do { const size_t __trait_s = (s); const char *const __trait_e = (e), *const __trait_a = (a); __traits_helper(__TRAITS_TO_STRING(eq(0, memcmp(e, a, s))), eq(0, memcmp(__trait_e, __trait_a, __trait_s)), __VA_ARGS__); } while(false)
+#define __assert_memory_equal_x(s, e, a, ...)       do { const size_t __trait_s = (s); const char *const __trait_e = (e), *const __trait_a = (a); __traits_helper(stringify(eq(0, memcmp(e, a, s))), eq(0, memcmp(__trait_e, __trait_a, __trait_s)), __VA_ARGS__); } while(false)
 #define __assert_memory_equal_0(s, e, a)            __assert_memory_equal_x(s, e, a, "Expected to be equal.\n")
 #define __assert_memory_equal_1(s, e, a, ...)       __assert_memory_equal_x(s, e, a, __VA_ARGS__)
 #define assert_memory_equal(...)                    __TRAITS_OVERLOAD_THREE(__assert_memory_equal_0, __assert_memory_equal_1, __VA_ARGS__)
 
-#define __assert_memory_not_equal_x(s, e, a, ...)   do { const size_t __trait_s = (s); const char *const __trait_e = (e), *const __trait_a = (a); __traits_helper(__TRAITS_TO_STRING(ne(0, memcmp(e, a, s))), ne(0, memcmp(__trait_e, __trait_a, __trait_s)), __VA_ARGS__); } while(false)
+#define __assert_memory_not_equal_x(s, e, a, ...)   do { const size_t __trait_s = (s); const char *const __trait_e = (e), *const __trait_a = (a); __traits_helper(stringify(ne(0, memcmp(e, a, s))), ne(0, memcmp(__trait_e, __trait_a, __trait_s)), __VA_ARGS__); } while(false)
 #define __assert_memory_not_equal_0(s, e, a)        __assert_memory_not_equal_x(s, e, a, "Expected to be not equal.\n")
 #define __assert_memory_not_equal_1(s, e, a, ...)   __assert_memory_not_equal_x(s, e, a, __VA_ARGS__)
 #define assert_memory_not_equal(...)                __TRAITS_OVERLOAD_THREE(__assert_memory_not_equal_0, __assert_memory_not_equal_1, __VA_ARGS__)
@@ -232,12 +227,12 @@ __traits_assert(bool condition, size_t line, const char *file, const char *asser
 /*
  * String
  */
-#define __assert_string_equal_x(e, a, ...)          do { const char *const __trait_e = (e), *const __trait_a = (a); __traits_helper(__TRAITS_TO_STRING(eq(0, strcmp(e, a))), eq(0, strcmp(__trait_e, __trait_a)), __VA_ARGS__); } while(false)
+#define __assert_string_equal_x(e, a, ...)          do { const char *const __trait_e = (e), *const __trait_a = (a); __traits_helper(stringify(eq(0, strcmp(e, a))), eq(0, strcmp(__trait_e, __trait_a)), __VA_ARGS__); } while(false)
 #define __assert_string_equal_0(e, a)               __assert_string_equal_x(e, a, "Expected to be equal.\n")
 #define __assert_string_equal_1(e, a, ...)          __assert_string_equal_x(e, a, __VA_ARGS__)
 #define assert_string_equal(...)                    __TRAITS_OVERLOAD_TWO(__assert_string_equal_0, __assert_string_equal_1, __VA_ARGS__)
 
-#define __assert_string_not_equal_x(e, a, ...)      do { const char *const __trait_e = (e), *const __trait_a = (a); __traits_helper(__TRAITS_TO_STRING(ne(0, strcmp(e, a))), ne(0, strcmp(__trait_e, __trait_a)), __VA_ARGS__); } while(false)
+#define __assert_string_not_equal_x(e, a, ...)      do { const char *const __trait_e = (e), *const __trait_a = (a); __traits_helper(stringify(ne(0, strcmp(e, a))), ne(0, strcmp(__trait_e, __trait_a)), __VA_ARGS__); } while(false)
 #define __assert_string_not_equal_0(e, a)           __assert_string_not_equal_x(e, a, "Expected to be not equal.\n")
 #define __assert_string_not_equal_1(e, a, ...)      __assert_string_not_equal_x(e, a, __VA_ARGS__)
 #define assert_string_not_equal(...)                __TRAITS_OVERLOAD_TWO(__assert_string_not_equal_0, __assert_string_not_equal_1, __VA_ARGS__)
