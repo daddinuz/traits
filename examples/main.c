98fdd8eae1bc33135a4bb2ffa99476bb8920a50a
daddinuz <daddinuz@gmail.com>
2019-11-04 16:06:10 +0100

    update license

--- examples/main.c
+++ examples/main.c
@@ -2,7 +2,7 @@
  * Author: daddinuz
  * email:  daddinuz@gmail.com
  *
- * Copyright (c) 2018 Davide Di Carlo
+ * Copyright (c) 2019 Davide Di Carlo
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
